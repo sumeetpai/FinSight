c1dff76e2ba301c4861a3d8a51bc6092ca1016f4
Sumeet Pai <sumeetpai2004@gmail.com>
2026-02-02 11:11:12 +0000

    base project structure

---
+++ FinSight-Backend/src/main/java/com/FinSight_Backend/FinSight_Backend/FinSightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.FinSight_Backend.FinSight_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSightBackendApplication.class, args);
+	}
+
+}
---
+++ FinSight-Backend/src/test/java/com/FinSight_Backend/FinSight_Backend/FinSightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinSight_Backend.FinSight_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
